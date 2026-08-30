01f988e9c6f65f911483caee6889cf353bc00fd5
helder virissio <helder.virissio@gmail.com>
2026-08-30 20:41:05 -0300

    feat: atualizacao paginas de disciplinas e estilos do projeto

--- app.js
+++ app.js
@@ -3,72 +3,86 @@ const http = require('http');
 const url  = require('url');
 const path = require('path');
 const fs   = require('fs');
- 
+
 const publicDir = path.join(__dirname, 'public');
- 
+
 // Content-Types:
 const contentTypes = {
     '.html':    'text/html; charset=utf-8',
     '.css':     'text/css; charset=utf-8',
     '.js':      'text/javascript; charset=utf-8',
     '.json':    'application/json; charset=utf-8',
+    '.jpg':     'image/jpeg',
     '.jpeg':    'image/jpeg',
     '.png':     'image/png',
     '.pdf':     'application/pdf',
     '.mp4':     'video/mp4'
 };
- 
+
 // Rotas:
 const routes = {
     '/': 'index.html',
-    '/algebra': 'algebra.html',
-    '/tp2': 'tp2.html',
-    '/dbnr': 'dadosNRel.html',
-    '/dw3': 'dw3.html',
+    '/algebra': 'algebraLinear.html',
+    '/dbnr': 'bancoDeDadosNaoRelacional.html',
+    '/dw3': 'desenvolvimentoWeb03.html',
     '/gestao-agil': 'gestaoAgil.html',
-    '/ihc': 'ihc.html',
+    '/ihc': 'interacaoHumanoComputador.html',
     '/ingles': 'ingles.html',
-    '/tp2': 'tp2.html',
+    '/tp2': 'tecnicasDeProgramacao02.html',
     '/grade': 'pdf/Matriz-curricular-Software-Multiplataforma-29-12-2021.pdf'
 };
- 
-// Abrir Arquivos:
-function readFile(response, file){
-    fs.readFile(file, function(err, data){
-        if(err){
-            response.writeHead(404, {'Content-Type':'text/html; charset=utf-8'});
-            return fs.createReadStream(
-                path.join(publicDir, 'erro404.html')
-            ).pipe(response);
+
+// Função para renderizar a página 404 estilizada contida em public/:
+function render404(response) {
+    const caminho404 = path.join(publicDir, 'erro404.html');
+    fs.readFile(caminho404, function(err, data) {
+        if (err) {
+            response.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+            response.end('<h1>404 - Página Não Encontrada</h1>');
+        } else {
+            response.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+            response.end(data);
         }
- 
+    });
+}
+
+// Abrir Arquivos:
+function readFile(response, file) {
+    fs.readFile(file, function(err, data) {
+        if (err) {
+            return render404(response);
+        }
+
         var extension = path.extname(file).toLowerCase();
         var contentType = contentTypes[extension] || 'application/octet-stream';
- 
-        response.writeHead(200, {'Content-Type':contentType});
+
+        response.writeHead(200, {'Content-Type': contentType});
         response.end(data);
     });
 }
- 
+
 // Funcao CallBack para utilizar no server http:
-var callback = function(request, response){
+var callback = function(request, response) {
     var pathname = decodeURIComponent(url.parse(request.url).pathname);
- 
-    // ROTAS:
-    if(routes[pathname])
+
+    // ROTAS (Procura todos os arquivos HTML/PDF dentro da pasta public):
+    if (routes[pathname]) {
         return readFile(response, path.join(publicDir, routes[pathname]));
- 
-    // Arquivos Estaticos:
+    }
+
+    // Arquivos Estaticos (CSS, JS, Imagens, Vídeo, etc.):
     var file = path.join(publicDir, pathname);
- 
+
     // Impedir acesso fora da pasta public:
-    if(!file.startsWith(publicDir))
-        return readFile(response, path.join(publicDir, 'erro404.html'));
- 
+    if (!file.startsWith(publicDir)) {
+        return render404(response);
+    }
+
     readFile(response, file);
-}
- 
+};
+
 // Servidor - Crie e Configura:
 var server = http.createServer(callback);
-server.listen(3000);
-console.log(`Servidor iniciado em http://localhost:3000/ ....`)
+server.listen(3000, function() {
+    console.log(`Servidor iniciado em http://localhost:3000/ ....`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,21 @@
+// Busca e insere os dados do aluno no rodapé
 fetch('/dados/aluno.json')
     .then(function(response){
+        if (!response.ok) {
+            throw new Error('Erro ao carregar os dados do aluno');
+        }
         return response.json();
     })
     .then(function(dados){
-        document.getElementById('alunoJSON').innerHTML =
-            '<b style="font-family: Manrope;">Nome:</b> ' + dados.nomes + '<br>' +
-            '<b style="font-family: Manrope;">Curso:</b> ' + dados.curso + '<br>' +
-            '<b style="font-family: Manrope;">Semestre:</b> ' + dados.semestre + '<br>' +
-            '<b style="font-family: Manrope;">Turno:</b> ' + dados.turno + '<br>'
+        var container = document.getElementById('alunoJSON');
+        if (container) {
+            container.innerHTML = 
+                '<b style="font-family: Manrope;">Nome:</b> ' + dados.nomes + '<br>' +
+                '<b style="font-family: Manrope;">Curso:</b> ' + dados.curso + '<br>' +
+                '<b style="font-family: Manrope;">Semestre:</b> ' + dados.semestre + '<br>' +
+                '<b style="font-family: Manrope;">Turno:</b> ' + dados.turno + '<br>';
+        }
+    })
+    .catch(function(erro){
+        console.error('Falha no fetch:', erro);
     });
